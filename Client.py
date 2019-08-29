f9fdcf23240899bb4d3514feb920e8976a7c604d
Regan <r.kouchoo1@gmail.com>
2019-08-29 12:30:21 +1000

    re-wrote socket code, it now recieves data properly

--- Client.py
+++ Client.py
@@ -1,21 +1,25 @@
-# client side server for recieveing data
-
+# load additional Python modules
 import socket
+import time
 
-host = 'localhost'
-port = 7990
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+local_hostname = socket.gethostname()
+local_fqdn = socket.getfqdn()
+ip_address = socket.gethostbyname(local_hostname)
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-s.connect((host, port))
+server_address = (ip_address, 1989)
+sock.connect(server_address)
+print ("connecting to %s (%s) with %s" % (local_hostname, local_fqdn, ip_address))
 
-s.listen(500)
+temperature_data = ["15", "22", "21", "26", "25", "19"]
 
-connection, addr = s.accept()
+for entry in temperature_data:
+    print ("data: %s" % entry)
+    new_data = str("temperature: %s\n" % entry).encode("utf-8")
+    sock.sendall(new_data)
+    
+    # wait for two seconds
+    time.sleep(2)
 
-recvData = []
-
-while connection.recv(1):
-	recvData.append(connection.recv(1))
-
-for data in recvData:
-	print(data)
+# close connection
+sock.close()
--- Server.py
+++ Server.py
@@ -1,21 +1,40 @@
-# Just a basic mock-up of how files can bre read and sent around to servers
-
+# load additional Python module
 import socket
 
-host = 'localhost'
-port = 7990
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+local_hostname = socket.gethostname()
+local_fqdn = socket.getfqdn()
+ip_address = socket.gethostbyname(local_hostname)
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-s.connect((host, port))
+# output hostname, domain name and IP address
+print ("working on %s (%s) with %s" % (local_hostname, local_fqdn, ip_address))
 
-f = open("testFile.txt", "rb")
+server_address = (ip_address, 1989)
+print ('starting up on %s port %s' % server_address)
+sock.bind(server_address)
 
-print("Sending data")
+# listen for incoming connections (server mode) with one connection at a time
+sock.listen(1)
 
-l = f.read()
+while True:
+    # wait for a connection
+    print ('waiting for a connection')
+    connection, client_address = sock.accept()
 
-for line in l:
-	s.send(line)
+    try:
+        # show who connected to us
+        print ('connection from', client_address)
 
-f.close()
-s.close()
+        # receive the data in small chunks and print it
+        while True:
+            data = connection.recv(64)
+            if data:
+                # output received data
+                print ("Data: %s" % data)
+            else:
+                # no more data -- quit the loop
+                print ("no more data.")
+                break
+    finally:
+        # Clean up the connection
+        connection.close()
