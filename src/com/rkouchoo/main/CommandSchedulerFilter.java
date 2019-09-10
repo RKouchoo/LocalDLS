70a4069b726d9f534a8b02d8ab39d5934a3148c6
Regan <r.kouchoo1@gmail.com>
2019-09-10 18:23:31 +1000

    basic structure for command interpreter

---
+++ src/com/rkouchoo/main/CommandSchedulerFilter.java
@@ -0,0 +1,35 @@
+package com.rkouchoo.main;
+
+public class CommandSchedulerFilter {
+
+	@SuppressWarnings("unused")
+	private enum COMMAND_SUBTYPES {
+		YAML,
+		S,
+		D,
+		L
+	}
+	
+	// Small state machine that is beign used
+	public enum EXEC_STATUS {
+		RUNNNING,
+		STOPPED,
+		STOPPING
+	}
+	
+	public static void pushCommandToThread(String command) {
+		
+	}
+	
+	public static void inspectYAMLFile(String filePath) {
+		
+	}
+	
+	public void startExternalCommandThread() {
+		
+	}
+	
+	public void stopExternalCommandThread() {
+		
+	}
+}
