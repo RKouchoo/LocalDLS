1cc8bd2fb25197f715d883e49fe745d30646d5c8
Regan <r.kouchoo1@gmail.com>
2019-11-11 16:35:51 +1100

    smbuilder work

--- src/smserver/smbuilder.py
+++ src/smserver/smbuilder.py
@@ -1,1 +1,43 @@
 # Class that loads and updates an SMList
+
+import yaml 
+import os
+
+import ccache
+
+# load, add, remove
+commands = ["l", "a", "r"]
+
+
+command = input("Please specify a command: ")
+
+
+while(True):
+
+	if command == "l":
+
+
+	if command == "a":
+
+
+	if command == "r":
+
+
+	if command not in commands:
+		print("Please specify a valid command!")
+		command = input("Please specify a command: ")
+
+
+# returns the yaml of the currentusercache
+def load(name):
+	fileData = open(name, "r")
+	return yaml.load(fileData.read())
+
+
+def save(name, sm):
+	fileData = yaml.dump(sm)
+
+	with open(filename, "w+") as textFile:
+		textFile.write(fileData)
+
+
